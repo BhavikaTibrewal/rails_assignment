5b6825116b4b468cc59d75152fbe88597767516c
BhavikaTibrewal <bhavika.t@browserstack.com>
2024-08-12 17:08:18 +0530

    assignment code

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root 'documents#index'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  # get '/profile' => 'users#show', :as => :user
+  get '/profile/edit' => 'users#edit'
+  patch '/profile' => 'users#update'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  resource  :document, except: [:new, :edit]
+end
